7f472fbc900ae051409e0bc79c242ee49714a0b0
thomalexg <thomas.gae@posteo.de>
2021-02-01 18:14:57 +0100

    Whatever, kinda works with 3 columns, want to change it to 2 now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+/** @jsxImportSource @emotion/react */ import { css } from '@emotion/react';
 import './App.css';
+
+const headerStyle = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  .box1-1 {
+    justify-self: start;
+  }
+  .box1-2 {
+    justify-self: end;
+  }
+  i {
+    display: inline;
+  }
+  h1 {
+    display: inline;
+  }
+`;
+const ss1 = css`
+  padding-top: 50px;
+  display: grid;
+  grid-template-columns: 40% 10% 50%;
+  img {
+    width: 90%;
+    margin-left: auto;
+  }
+  .pic-1 {
+    grid-column: 1/3;
+    grid-row: 1;
+    text-align: right;
+  }
+  .s1text {
+    grid-column: 2/4;
+    grid-row: 1;
+    padding-top: 25%;
+    padding-left: 5.5rem;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div css={headerStyle} className="App-header container-1">
+        <div className="box1-1">
+          <i class="fab fa-pagelines fa-3x"></i>
+          <h1>Cloner</h1>
+        </div>
+        <form className="box1-2">
+          <input
+            type="email"
+            placeholder="Enter mail to subscribe, asshole!"
+          ></input>
+          <button>Subscribe!</button>
+        </form>
+      </div>
+      <div id={1} css={ss1}>
+        <div className="pic-1">
+          <img src="pics/tabphone-s-1.jpg" alt="Tablet and phone"></img>
+        </div>
+        <div className="grid-item-1 s1text">
+          <h2>The all-in-one notebook for creatives.</h2>
+          <p>Brainstorm, write, and plan in a single tool.</p>
+        </div>
+      </div>
+      <div id={2}>Section 2</div>
+      <div id={3}>Section 3</div>
+      <div id={4}>Section 4</div>
+      <div id={5}>Section 5</div>
+      <div id={6}>Section 6</div>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const globalStyles = css`
+  background-color: #212224;
+  margin: 0;
+  padding: 0;
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
